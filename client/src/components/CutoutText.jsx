b256405f6f63411f39ff749de014088843986f26
G0dKing <ajcaskey@gmail.com>
2024-04-26 20:24:53 -0400

    Updated CutoutText.jsx

--- client/src/components/CutoutText.jsx
+++ client/src/components/CutoutText.jsx
@@ -4,7 +4,7 @@ import '@style/CutoutText.css'
 const CutoutText = () => {
     return (
       <>
-        <h2 className="white heading">JOIN OUR MAILING LIST</h2>
+       <button><h2 className="white heading">JOIN OUR MAILING LIST</h2></button>
       </>
     );
 }
