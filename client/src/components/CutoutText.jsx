c4525a4adee16d275cf00269babc2e6e51196aa2
G0dKing <ajcaskey@gmail.com>
2024-04-26 20:25:59 -0400

    Rolled back changes to CutoutText.jsx

--- client/src/components/CutoutText.jsx
+++ client/src/components/CutoutText.jsx
@@ -4,7 +4,7 @@ import '@style/CutoutText.css'
 const CutoutText = () => {
     return (
       <>
-       <button><h2 className="white heading">JOIN OUR MAILING LIST</h2></button>
+     <h2 className="white heading">JOIN OUR MAILING LIST</h2>
       </>
     );
 }
