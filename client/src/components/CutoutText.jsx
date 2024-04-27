11b0b00bc8274b2255d2c024a9678c46d3b5832b
G0dKing <ajcaskey@gmail.com>
2024-04-26 21:36:37 -0400

    Corrected Modal Logic

--- client/src/components/CutoutText.jsx
+++ client/src/components/CutoutText.jsx
@@ -1,12 +1,17 @@
 import { } from 'react'
+import PropTypes from 'prop-types'
 import '@style/CutoutText.css'
 
-const CutoutText = () => {
+const CutoutText = ({ onClick }) => {
     return (
       <>
-     <h2 className="white heading">JOIN OUR MAILING LIST</h2>
+     <h2 className="white heading" onClick={onClick}>JOIN OUR MAILING LIST</h2>
       </>
     );
 }
+
+CutoutText.propTypes = {
+  onClick: PropTypes.func.isRequired,
+};
 
 export default CutoutText
